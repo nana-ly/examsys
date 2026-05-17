089c051f19d0cc2b4bbc939cdfc770409ab5da7f
suerte <960921483@qq.com>
2026-05-17 13:58:06 +0800

    清理：移除嵌套项目副本和IDE配置文件
    - 删除合并引入的嵌套项目目录 exam-back-b4473724c1584f32dcaec932d158f23911de2442/
    - 删除误提交的 .idea/ IDE配置文件
    - 还原 test_auto_generate.py 到版本库状态

--- exam-back-b4473724c1584f32dcaec932d158f23911de2442/question_bank/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Question
-
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ['content', 'question_type', 'difficulty', 'knowledge_point', 'creator']
-    list_filter = ['question_type', 'difficulty']
-    search_fields = ['content', 'knowledge_point']
